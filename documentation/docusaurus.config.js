d25d6b7e003a26f0625759d2bcdd98150445f640
Tobias Ernst <tobi@legartis.ai>
2023-07-27 11:01:29 +0200

    Github pages deployment

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon-white.svg',
 
   // Set the production url of your site here
-  url: 'https://docs.legartis.ai',
+  url: 'https://legartistech.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/documentation/',
